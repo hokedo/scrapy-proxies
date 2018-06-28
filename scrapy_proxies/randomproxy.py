06827722b8ba00621a91d2e5806a9c5c289bf85f
Bogdan Mic <bogdan.mic@trustyou.net>
2018-06-28 22:02:21 +0300

    Fixed bug when not providing user and password

--- scrapy_proxies/randomproxy.py
+++ scrapy_proxies/randomproxy.py
@@ -91,16 +91,16 @@ class RandomProxy(object):
         else:
             proxy_address = self.chosen_proxy
 
+        request.meta['proxy'] = proxy_address
         proxy_user_pass = self.proxies[proxy_address]
 
         if proxy_user_pass:
-            request.meta['proxy'] = proxy_address
             basic_auth = 'Basic ' + base64.b64encode(proxy_user_pass.encode()).decode()
             request.headers['Proxy-Authorization'] = basic_auth
         else:
             log.debug('Proxy user pass not found')
         log.debug('Using proxy <%s>, %d proxies left' % (
-                proxy_address, len(self.proxies)))
+            proxy_address, len(self.proxies)))
 
     def process_exception(self, request, exception, spider):
         if 'proxy' not in request.meta:
